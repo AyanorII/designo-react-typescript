26e97f39939590a651d5cb91a868d13d3df11d60
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-03-27 21:00:53 +0900

    added click away on header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,34 +1,41 @@
-import { useState, useEffect } from 'react'
-import styled from 'styled-components';
-import Logo from '../Logo/Logo';
-import Nav from '../Nav/Nav';
-import NavButton from '../Nav/NavButton';
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+import ClickAwayListener from "react-click-away-listener";
+import Logo from "../Logo/Logo";
+import Nav from "../Nav/Nav";
+import NavButton from "../Nav/NavButton";
 
 interface Props {
   isNavOpen: boolean;
 }
 
 const Header = () => {
-  const [isNavOpen, setIsNavOpen] = useState(false)
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
   useEffect(() => {
-    document.body.style.overflow = isNavOpen ? 'hidden' : 'unset'
-  }, [isNavOpen])
+    document.body.style.overflow = isNavOpen ? "hidden" : "unset";
+  }, [isNavOpen]);
 
   const toggleMenu = (): void => {
-    setIsNavOpen(prev => !prev)
+    setIsNavOpen((prev) => !prev);
+  };
+
+  const closeMenu = (): void => {
+    setIsNavOpen(false)
   }
 
   return (
-    <StyledHeader isNavOpen={isNavOpen}>
-      <Logo variant="dark" />
-      <Nav isNavOpen={isNavOpen}/>
-      <NavButton isNavOpen={isNavOpen} toggleMenu={toggleMenu} />
-    </StyledHeader>
-  )
-}
+    <ClickAwayListener onClickAway={closeMenu}>
+      <StyledHeader isNavOpen={isNavOpen}>
+        <Logo variant="dark" />
+        <Nav isNavOpen={isNavOpen} />
+        <NavButton isNavOpen={isNavOpen} toggleMenu={toggleMenu} />
+      </StyledHeader>
+    </ClickAwayListener>
+  );
+};
 
-export default Header
+export default Header;
 
 const StyledHeader = styled.header<Props>`
   background-color: ${(props) => props.theme.colors.primary.light};
@@ -41,4 +48,12 @@ const StyledHeader = styled.header<Props>`
   box-shadow: 10px 30px 30px 100vh
     ${(props) => (props.isNavOpen ? "rgba(0, 0, 0, 0.75)" : "rgba(0, 0, 0, 0)")};
   transition: box-shadow 0.35s ease-in-out;
+
+  @media (min-width: 768px) {
+    padding: 4rem 2.5rem;
+  }
+
+  @media (min-width: 1440px) {
+    padding: 4rem 10.3125rem;
+  }
 `;
