bb4bddc45725ba4a2f9529294ef2fadf3345e2ce
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-06-25 15:41:30 +0900

    fixed google analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import GA4React from "ga-4-react";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
+console.log(process.env.GOOGLE_ANALYTICS_ID);
+
+try {
+  setTimeout(() => {
+    const ga4react = new GA4React("G-YRV6KLTJKH");
+    ga4react.initialize().catch((err) => console.error(err));
+  }, 4000);
+} catch (err) {
+  console.error(err);
+}
